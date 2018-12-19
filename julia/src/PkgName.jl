0e34540fbaa3d4930376b40dbd9f1e5d3367bf0b
Ryan Price <ryapric@gmail.com>
2018-12-19 17:40:06 -0600

    Refactor include()s in main module & tests to be less verbose

--- julia/src/PkgName.jl
+++ julia/src/PkgName.jl
@@ -3,13 +3,17 @@ This is a main module file. In it, you should:
 
 - `export` the desired functions from your `include()` calls (comma-delimited)
 
-- `include()` your `srcfile`s
+- `include()` your `srcfile`s. There is a stubbed, vectorized `include()` here,
+  but if you want to change it, just construct a manual array of source file
+  names.
 """
 
 module PkgName
 
 export sharpe
 
-include("./srcfile.jl")
+dir = readdir(dirname(@__FILE__))
+includes = filter(x -> (endswith(x, "jl") && x != "PkgName.jl"), dir)
+include.(includes)
 
-end # module PkgName
+end # module Ex
--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -1,35 +1,9 @@
 """
-This file structure taken from the DataFrames runtests.jl file. As you add
-test files, add them to the `tests` array. Note that `srcfile.jl` is just a
-placeholder name for actual source files.
-
-This file will be called when running `make test` from the repo top-level.
+This file will be called when running `make test` from the repo top-level. It
+reads all the ".jl" files in the test directory into an array, and runs
+`include()` on each of them.
 """
 
-fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
-quiet = length(ARGS) > 0 && ARGS[1] == "-q"
-anyerrors = false
-
-tests = ["srcfile.jl"]
-
-println("Running tests:")
-
-for testname in tests
-    try
-        include(testname)
-        println("\t\033[1m\033[32mPASSED\033[0m: $(testname)")
-    catch e
-        global anyerrors = true
-        println("\t\033[1m\033[31mFAILED\033[0m: $(testname)")
-        if fatalerrors
-            rethrow(e)
-        elseif !quiet
-            showerror(stdout, e, backtrace())
-            println()
-        end
-    end
-end
-
-if anyerrors
-    throw("Tests failed")
-end
+dir = readdir(dirname(@__FILE__))
+tests = filter(x -> (endswith(x, "jl") && x != "runtests.jl"), dir)
+include.(tests)
