f625effcb852683bfb565d976c2330ae9d96aee5
Ryan Price <ryapric@gmail.com>
2018-12-19 16:00:16 -0600

    Add actual Julia install script.

---
+++ julia/install-julia.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Must be run as root. Aborting" >&2
+    exit 1
+fi
+
+if [ -z "$1" ] ; then
+    echo "Please provide the version number as the first arg, e.g. '1.0.0'. Aborting." >&2
+    exit 1
+fi
+
+tmpgz="/tmp/julia.tar.gz"
+julia="/tmp/julia-${1}"
+
+curl -o "$tmpgz" "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-${1}-linux-x86_64.tar.gz"
+
+mkdir -p /usr/share/julia
+tar --overwrite -xzf "$tmpgz" -C /usr/share/julia/
+
+ln -s -f /usr/share/julia/julia-${1}/bin/julia /usr/bin/julia
+
+rm -rf "$tmpgz"
+
+exit 0
