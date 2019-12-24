b1e7e991eaf264108410bd46444d30e1a30cddbe
Ryan Price <ryapric@gmail.com>
2019-12-24 12:06:43 -0600

    Changes to Docker deb-sysinit.sh, and make it install docker-compose to a venv

--- docker/deb-sysinit.sh
+++ docker/deb-sysinit.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
 # This script will intialize system requirements for a Debian-based Docker host.
-# Specifically, the system needs:
+# Specifically, the system will receive:
 #   - Docker
-#   - Docker Compose (and pip3 to install it, if not present)
+#   - Docker Compose (and python3 utilities to install it, if not present)
+#
+# Sometimes the host will throw segfaults on Docker Compose install, so it will
+# be installed to a virtual environment named `venv` in the parent directory you
+# specify as the first CLI arg.
 
 # Pin docker-compose version with a leading major-minor, to prevent any breakage
 export DOCKER_COMPOSE_VER=1.24
+
+# docker-compose venv path. Defaults to `.`
+if [[ -z "$1" ]]; then
+  export VENV_HOME='.'
+else
+  export VENV_HOME="$1"
+fi
 
 # Install Docker, if it's not already installed
 if ! command -v docker >/dev/null; then
@@ -16,13 +28,19 @@ fi
 
 # Install docker-compose, and pip3 if needed
 if ! command -v pip3; then
-  sudo apt-get update && sudo apt-get install -y python3 python3-pip
+  sudo apt-get update && sudo apt-get install -y \
+    python3 \
+    python3-pip \
+    python3-venv
 fi
 if ! command -v docker-compose; then
   sudo apt-get install -y \
     libffi-dev \
     libssl-dev
+  python3 -m venv "${VENV_HOME}/venv"
+  source "${VENV_HOME}/venv/bin/activate"
   pip3 install "docker-compose==${DOCKER_COMPOSE_VER}.*"
+  deactivate
 fi
 
 # Woo!
